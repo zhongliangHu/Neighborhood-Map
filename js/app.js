bfc0d31c850897d9422ad9ab3bc7a34cbf47c2af
huzhongliang <huzl0525@163.com>
2017-07-22 22:38:42 +0800

    app.js

--- js/app.js
+++ js/app.js
@@ -37,3 +37,107 @@ function init(){
 	});
 	model.infoWindow = new BMap.InfoWindow();
 }
+
+function convert(point,cb){
+	var convertor=new BMap.Convertor();
+	var pointArr=[];
+	pointArr.push(point);
+	convertor.translate(pointArr,3,5,cb);
+}
+
+//function initMarker() 在Google.maps.Marker类的基础上进行扩展 添加了Marker的放大缩小功能，infoWindow的弹出功能
+// @param{*} proto
+
+function initMarker(proto){
+	proto.setLarge=function(){
+		this.selected = true;
+		window.setTimeout(function(){
+			if(this.selected){
+				this.getMap().panTo(this.position);
+				this.showLabel();
+				this.setAnimation(BMAP_ANIMATION_BOUNCE);
+			}
+		}.bind(this),200);
+	}；
+	proto.showLabel=function(){
+		this.setTop(true);
+		this.setLabel(new BMap.Label(this.title,{offset:new BMap.Size(20,-10)}));
+	};
+	proto.setNormal=function(){
+		this.selected=false;
+		this.setAnimation(null);
+		this.setTop(false);
+		this.getMap().removeOverlay(this.getLabel());
+	};
+	proto.openWindow=function(){
+		model.infoWindow.setContent(this.stationInfo);
+		this.openInfoWindow(model.infoWindow);
+		this.windowOpen=true;
+	};
+	proto.toggleWindow = function(){
+		if(window.clickDelay)
+			return;
+		window.clickDelay=true;
+		window.setTimeout(function(){
+			window.clickDelay=false;
+			if(this.windowOpen){
+				this.closeInfoWindow();
+				return;
+			}
+			this.openWindow();
+		}.bind(this),200);
+	};
+	proto.setWindowClose=function(){
+		this.windowOpen=false;
+	};
+	
+}
+
+function toggleside(){
+	$(".container").toggleClass)("side-hide");
+}
+
+//实现bus stop 信息的查询 完成Marker的初始化
+//@param{*} items
+
+function loadData(items){
+	map.clearOverlays();
+	items.removeAll();
+	var options = {
+		pageCapacity: 10,
+		onSearchComplete:onSucess
+	};
+	var local = new BMap.LocalSearch(map,options);
+	local.searchInBounds("公交站",map.getBounds());
+	function onSucess(results){
+		if(local.getStatus() == BMap_STATUS_SUCCESS){
+			//判断状态是否正确
+			setMarker(results);
+			var nextPage = results.getPageIndex()+1;
+			if(nextPage < results.getNumPages())
+				local.gotoPage(nextPage);
+		}
+	}
+	function setMarker(results){
+		var curPosi;
+		for (var i=0; i<results.getCurrentNumPois(); i++){
+			// s.push(results.getPoi(i).title + ", " + results.getPoi(i).address); 
+			curPosi = results.getPoi(i);
+			
+			var marker = new BMap.Marker(curPosi.point);
+			marker.title = curPosi.title;
+			marker.visible = true;
+			marker.selected = false;
+			marker.stationInfo = curPosi.address;
+			marker.position = curPosi.
+		}
+		
+	}
+}
+
+
+
+
+
+
+
